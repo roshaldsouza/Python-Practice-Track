632914f1642789f5048734c4d16ac43dc29b8216
RoshalDsouza24 <roshalds789@gmail.com>
2026-08-19 18:46:10 +0530

    tech method

--- course.py
+++ course.py
@@ -6,12 +6,18 @@ class Course:
         self.technologies = technologies
         self.start_date = start_date
     def display_course(self):
-        print(f"Course Name - {name} , Duration - {duration} , Trainer Name - {trainer_name}, Technologies - {technolgies}, Start Date - {start_date} ")
+        print(f"Course Name - {self.name} , Duration - {self.duration} , Trainer Name - {self.trainer_name}, Technologies - {self.technologies}, Start Date - {self.start_date} ")
+    def is_tech_covered(self,technology):
+        if technology in self.technologies:
+            return True
+        else:
+            return False
 name = input("enter your name ")
 duration = int(input("enter duration "))
 trainer_name = input("enter trainer name ")
-technolgies = list(map(str,input("enter technolgies ").split()))
+technologies = list(map(str,input("enter technologies ").split()))
 start_date = input("enter date ")
 
-c = Course(name,duration,trainer_name,technolgies,start_date)
+c = Course(name,duration,trainer_name,technologies,start_date)
 c.display_course()
+print(c.is_tech_covered("mongo"))

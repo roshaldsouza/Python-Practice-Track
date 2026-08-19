9b2a4a1fb63f376328ac3aa05b48c5fa635b14d3
RoshalDsouza24 <roshalds789@gmail.com>
2026-08-19 18:25:48 +0530

    update printing format

--- course.py
+++ course.py
@@ -5,15 +5,13 @@ class Course:
         self.trainer_name = trainer_name
         self.technologies = technologies
         self.start_date = start_date
+    def display_course(self):
+        print(f"Course Name - {name} , Duration - {duration} , Trainer Name - {trainer_name}, Technologies - {technolgies}, Start Date - {start_date} ")
 name = input("enter your name ")
 duration = int(input("enter duration "))
 trainer_name = input("enter trainer name ")
 technolgies = list(map(str,input("enter technolgies ").split()))
 start_date = input("enter date ")
-c = Course(name,duration,trainer_name,technolgies,start_date)
-print("name ",c.name)
-print("duration",c.duration)
-print("trainer name",c.trainer_name)
-print("technolgies",c.technologies)
-print("start date",c.start_date)
 
+c = Course(name,duration,trainer_name,technolgies,start_date)
+c.display_course()
